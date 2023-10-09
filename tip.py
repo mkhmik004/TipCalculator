ec7977fe81ae8e225174171b047ea2d3c461ce7d
Mkhabele Mikejunior Mark Mashaole <137772011+mkhmik004@users.noreply.github.com>
2023-10-09 13:10:01 +0200

    Add files via upload

---
+++ tip.py
@@ -0,0 +1,15 @@
+#MKHABELE MMM
+#28/06/2023
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    return float(d[1::])
+def percent_to_float(p):
+    p=p[:-1]
+    return float(p)/100
+main()

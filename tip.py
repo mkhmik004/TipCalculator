f4907a2d8c838d2a5a9f13d1f6693cd4bc8cb059
Mkhabele Mikejunior Mark Mashaole <137772011+mkhmik004@users.noreply.github.com>
2023-10-09 13:11:49 +0200

    Update tip.py

--- tip.py
+++ tip.py
@@ -3,13 +3,13 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
+    tip = dollars * percent #conversion
     print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
-    return float(d[1::])
+    return float(d[1::])#removes the currency
 def percent_to_float(p):
-    p=p[:-1]
+    p=p[:-1]#removes the percentage sign
     return float(p)/100
 main()
